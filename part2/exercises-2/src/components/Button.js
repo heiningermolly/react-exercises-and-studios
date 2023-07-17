c0c3aa9cf8e8f0f57b3995c920f2259eabc5ffcf
Molly Heininger <heininger.molly@gmail.com>
2023-07-17 11:54:33 -0500

    finished

--- part2/exercises-2/src/components/Button.js
+++ part2/exercises-2/src/components/Button.js
@@ -2,8 +2,12 @@ import './styles.css';
 
 function Button() {
 
+   const onLearnMore = () => (
+   window.alert("The life in everything yearns for the death of capitalism. ORCANIZE!")
+   );
+
    return ( 
-         <button>
+         <button onClick={onLearnMore}>
             Learn More
          </button>
    );
