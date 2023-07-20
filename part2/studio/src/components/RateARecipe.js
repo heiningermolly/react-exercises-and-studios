1cfc3b97f63497dc06a72ae060d3eb27b5035063
Molly Heininger <heininger.molly@gmail.com>
2023-07-19 20:14:33 -0500

    getting started on part 2

--- part2/studio/src/components/RateARecipe.js
+++ part2/studio/src/components/RateARecipe.js
@@ -1,6 +1,9 @@
 let stars = ["⭐", "⭐ ⭐", "⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐ ⭐"];
 
 function RateARecipe() {
+  const GiveRating = () => {
+    props.rating < 1 || props.rating > 5 ? return null : return <h3>{stars[props.rating - 1]}</h3>;
+  }
   return stars;
 }
 
